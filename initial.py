45526736914f46c7a66b8f71404595c9842b056a
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2024-01-09 09:39:35 +0000

    Initial commit

---
+++ initial.py
@@ -0,0 +1,12 @@
+
+def odd_and_even(num):
+    if num % 2 == 0:
+      print("This is an even number")
+
+    else:
+      print("This is an odd number")
+
+    return num
+
+num = int(input("Enter a number: "))
+odd_and_even(num)
