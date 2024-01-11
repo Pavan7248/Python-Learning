e25ae115990ecfd48da2fbfc0021848c0756d0d6
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2024-01-11 07:01:37 +0000

    Modified

--- initial.py
+++ initial.py
@@ -1,12 +1,15 @@
-
 def odd_and_even(num):
-    if num % 2 == 0:
-      print("This is an even number")
+  if num % 2 == 0:
+    print("This is an even number")
 
-    else:
-      print("This is an odd number")
+  else:
+    print("This is an odd number")
 
-    return num
+  return num
+
 
 num = int(input("Enter a number: "))
 odd_and_even(num)
+
+number = int(input("Enter a number: "))
+odd_and_even(number)
--- list.py
+++ list.py
@@ -1,5 +1,6 @@
 list1 = [1, 2, 3, 4, 5 ]
 list2 = [6, 7, 8, 9, 10]
+#.append() adds the element to the end of the list
 list1.append(list2)
 print(list1)
 #Removing extra square brackets from the list i.e. [1, 2, 3, 4, 5, [6, 7, 8, 9, 10]]
@@ -7,4 +8,8 @@ print(list1)
 
 nested_list = list1.pop()
 list1.extend(nested_list)
+print(list1)
+
+# .extend() function adds the elements of a list (or any iterable), to the end of the current list.
+list1.extend(list2)
 print(list1)
