290862213a745353188717bf14a4e63913ec47d0
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2024-01-12 12:33:40 +0000

    Added a program to reverse the given number

--- list.py
+++ list.py
@@ -13,3 +13,30 @@ print(list1)
 # .extend() function adds the elements of a list (or any iterable), to the end of the current list.
 list1.extend(list2)
 print(list1)
+# .insert() function inserts the specified value at the specified position.
+list1.insert(0, 0)
+print(list1)
+
+#.remove() function removes the first occurrence of the element with the specified value.
+list1.remove(0)
+print(list1)
+
+#.reverse() function reverses the order of the elements in the list.
+list1.reverse()
+print(list1)
+
+#.sort() function sorts the list in ascending order
+list1.sort()
+print(list1)
+
+#.sort(reverse=True) function sorts the list in descending order
+list1.sort(reverse=True)  
+print(list1 )
+
+#.count() function returns the number of elements with the specified value.
+print(list1.count(1))
+
+#.index() function returns the index of the first element with the specified value.
+print(list1.index(1))
+#.copy() function returns a copy of the list
+
---
+++ reverse.py
@@ -0,0 +1,11 @@
+#Program to display the reverse of a given number.
+num = int(input("Enter a number: "))
+
+rev = 0
+while num > 0:
+  rem = num % 10
+  rev = rev * 10 + rem
+  num = num // 10
+
+print("The reverse of the number is: ", rev)
+
