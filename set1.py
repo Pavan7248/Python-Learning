e7cafc366a124997abf1ec3ccbafd75f8b836ca3
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2024-06-20 06:01:11 +0000

    set program

--- set1.py
+++ set1.py
@@ -4,3 +4,29 @@ set1 = {10, 20, 30, 40, 50}
 set2 = {30, 40, 50, 60, 70}
 
 print(set1.symmetric_difference(set2))
+
+
+#Slice list into 3 equal chunks and reverse each chunk
+
+sample_list = [11, 45, 8, 23, 14, 12, 78, 45, 89]
+print("Original list ", sample_list)
+
+length = len(sample_list)
+chunk_size = int(length / 3)
+start = 0
+end = chunk_size
+
+# run loop 3 times
+for i in range(3):
+    # get indexes
+    indexes = slice(start, end)
+
+    # get chunk
+    list_chunk = sample_list[indexes]
+    print("Chunk ", i, list_chunk)
+
+    # reverse chunk
+    print("After reversing it ", list(reversed(list_chunk)))
+
+    start = end
+    end += chunk_size
